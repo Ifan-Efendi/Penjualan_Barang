9839002bc5cdd615e6a984bf16de3bbad577741c
unknown <ifanefendi666@gmail.com>
2024-07-02 18:01:54 +0700

    first commit

---
+++ src/penjulalan_barang/txt_password.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package penjulalan_barang;
+
+/**
+ *
+ * @author Ifan
+ */
+class txt_password {
+    
+}
